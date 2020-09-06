477ab013ac3336f109e77f140c0027a574a18f13
Abdulaziz Yahya <abdulaziz.y22@gmail.com>
2020-09-06 21:13:01 +0300

    First

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="component">
+    <Header :quoteCount="quotes.length" :maxQuotes="maxQuotes" />
+    <NewQuote @quoteAdded="newQuote"></NewQuote>
+    <QuoteGrid :quotes="quotes" @quoteDeleted="deleteQuote" />
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">Info: Click on a Quote to Delete it!</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import QuoteGrid from './components/QuoteGrid.vue'
+import NewQuote from './components/NewQuote.vue'
+import Header from './components/Header.vue'
+
+export default {
+  data: function(){
+    return{
+      quotes:['A quote'],
+      maxQuotes: 10
+    }
+  },
+  methods: {
+    newQuote(quote){
+      if(this.quotes.length >= this.maxQuotes){
+        return alert('Please delete Quotes first!')
+      }
+      this.quotes.push(quote)
+    },
+    deleteQuote(index){
+      this.quotes.splice(index, 1)
+    }
+  },
+  components: {
+    QuoteGrid,
+    NewQuote,
+    Header
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="row">
+      <Quote 
+        v-for="(quote, index) in quotes" 
+        :key="quote.id"
+        @click.native="deleteQuote(index)"
+        >{{quote}}</Quote>
+  </div>
+</template>
+
+<script>
+import Quote from './Quote.vue'
+export default {
+    props: ['quotes'],
+    methods: {
+        deleteQuote(index){
+            this.$emit('quoteDeleted', index)
+        }
+    },
+    components: {
+        Quote
+    }
+}
+</script>
+
+<style>
+
+</style>
